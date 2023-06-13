996fc261f1a23ec5b879bd9c134aa7f05ed14821
daniel jermaine <danieljermaine2@gmail.com>
2023-06-13 02:44:47 +0100

    Initial Commit

---
+++ Spark/SparkApp.swift
@@ -0,0 +1,17 @@
+//
+//  SparkApp.swift
+//  Spark
+//
+//  Created by Apple on 13/06/2023.
+//
+
+import SwiftUI
+
+@main
+struct SparkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
